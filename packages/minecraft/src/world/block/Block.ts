6db24db6d98fca5dfd1163f986c6eada5c204c79
ramenha0141 <ramenhaoishii@gmail.com>
2023-03-03 15:52:34 +0900

    [minecraft]

--- packages/minecraft/src/world/block/Block.ts
+++ packages/minecraft/src/world/block/Block.ts
@@ -5,13 +5,17 @@ export default class Block {
     public static readonly id: string = '';
     public static readonly texture: BlockTexture;
 
+    public getId(): string {
+        return (this.constructor as typeof Block).id;
+    }
+
     public getTexture(): BlockTexture {
         return (this.constructor as typeof Block).texture;
     }
 }
 
 declare module './BlockRegistry' {
-    interface BlockIds {
+    interface BlockId {
         block: Block;
     }
 }
--- packages/minecraft/src/world/block/BlockRegistry.ts
+++ packages/minecraft/src/world/block/BlockRegistry.ts
@@ -1,11 +1,24 @@
-export interface BlockIds {}
+export interface BlockId {}
 
 export default class BlockRegistry {
-    static blockIDs: Set<string>;
+    static blockIds: (keyof BlockId)[];
 
-    static register<T extends keyof BlockIds>(id: T) {
+    static register<T extends keyof BlockId>(id: T) {
         if (!/^[a-z_]+:[a-z_]+$/.test(id)) throw new Error();
 
-        this.blockIDs.add(id);
+        if (this.blockIds.includes(id)) throw new Error();
+
+        this.blockIds.push(id);
+    }
+
+    static get<T extends keyof BlockId>(index: number): keyof BlockId | null;
+    static get<T extends keyof BlockId>(id: T): number | null;
+    static get<T extends keyof BlockId>(indexOrId: number | T) {
+        if (typeof indexOrId === 'number') {
+            return this.blockIds[indexOrId] ?? null;
+        } else {
+            const index = this.blockIds.indexOf(indexOrId);
+            return index !== -1 ? index : null;
+        }
     }
 }
