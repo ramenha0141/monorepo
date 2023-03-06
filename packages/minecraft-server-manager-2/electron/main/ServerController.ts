50ada56bccd50792e07d7d3f3ccdc5cfc06f9bbd
ramenha0141 <ramenhaoishii@gmail.com>
2023-03-06 14:48:09 +0900

    [minecraft-server-manager-2]

--- packages/minecraft-server-manager-2/electron/main/ServerController.ts
+++ packages/minecraft-server-manager-2/electron/main/ServerController.ts
@@ -43,16 +43,9 @@ class ServerController {
     }
     async start(callback: (data: string) => void): Promise<boolean> {
         this.isProcessing = true;
-        this.process = spawn(
-            'java',
-            [
-                '-Xms2G -Xmx2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true',
-                '-jar',
-                'server.jar',
-                '-nogui',
-            ],
-            { cwd: this.path },
-        );
+        this.process = spawn('java', ['-Xms2G -Xmx2G', '-jar', 'server.jar', '-nogui'], {
+            cwd: this.path,
+        });
         this.process.stdout.pipe(process.stdout);
         this.process.stdout.on('data', (data: Buffer) => {
             const str = data.toString();
